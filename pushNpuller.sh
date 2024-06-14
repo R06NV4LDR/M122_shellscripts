b1eafba00ee4247c22a4756747dee2de0a0b2e2e
rooney <ronny.bruhin@edu.tbz.ch>
2024-06-14 10:24:54 +0200

    2024-06-14 10:24:53 | M122 Scripts (Auto Update) | Last File: pushNpuller.sh

--- pushNpuller.sh
+++ pushNpuller.sh
@@ -5,24 +5,64 @@ current_date=$(date '+%Y-%m-%d %H:%M:%S')
 echo "Das aktuelle Datum: $current_date"
 
 # Verzeichnis wechseln
-cd ~/scripts/ || { echo "Verzeichnis ~/scripts/ nicht gefunden"; exit 1; }
-echo "Change directory to scripts/"
+if cd ~/scripts/; then
+	echo "Changed directory to ~/scripts/" >> ~/Docs/logs/autogit.log
+else
+	echo "Couldn't find directory ~/scripts/" >> ~/Docs/logs/autogit.log
+	exit 1
+fi
 
-# Pullt Änderungen aus dem Remote Repo
-echo "Pull Date: $current_date" >> ~/Docs/logs/autogit.log
-git pull
+# Findet die zuletzt erstellte Datei
 
-# Änderungen zu git hinzufügen
-git add .
-echo "Added all to git"
 
-# Commit mit Datum und Nachricht erstellen
-commit_msg="$current_date | M122 | Scripts (Auto Update)"
-git commit -m "$commit_msg"
-echo "Added commit message: $commit_msg"
+last_created_file=$(ls -t | head -n 1)
+echo "Last created file: $last_created_file" >> ~/Docs/logs/autogit.log
 
-# Änderungen in das Repository pushen
-echo "Push Date: $current_date | Message: $commit_msg" >> ~/Docs/logs/autogit.log
-git push
+# Pullt �Aenderungen aus dem Remote Repo
+{
+	echo "Pull Date: $current_date"
+	git pull
+} >> ~/Docs/logs/autogit.log 2>&1
+
+
+if [ $? -ne 0 ]; then
+	echo "Error pulling changes from remote repo" >> ~/Docs/logs/autogit.log
+	exit 1
+fi
+
+
+# Aenderungen zu git hinzufuegen
+{
+	git add .
+	echo "Added all changes to git"
+} >> ~/Docs/logs/autogit.log 2>&1
+
+if [ $? -ne 0 ]; then
+	echo "Error adding changes to git" >> ~/Docs/logs/autogit.log
+	exit 1
+fi
+
+# Commit mit Datum und und neuestem Skriptnamen erstellen
+commit_msg="$current_date | M122 Scripts (Auto Update) | Last File: $last_created_file"
+{
+	git commit -m "$commit_msg"
+	echo "Added commit message: $commit_msg"
+} >> ~/Docs/logs/autogit.log 2>&1
+
+if [ $? -ne 0 ]; then
+	echo "Error comitting changes" >> ~/Docs/logs/autogit.log
+	exit 1
+fi
+
+# �Aenderungen in das Repository pushen
+{
+	echo "Push Date: $current_date | Message: $commit_msg"
+	git push
+} >> ~/Docs/logs/autogit.log 2>&1
+
+if [ $? -ne 0 ]; then
+	echo "Error pushing changes to remote repo" >> ~/Docs/logs/autogit.log
+	exit 1
+fi
 
 
