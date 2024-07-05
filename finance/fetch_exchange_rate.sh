532cd4302cf3c405683ffe296c154fd93b389e32
rooney <ronny.bruhin@edu.tbz.ch>
2024-07-05 10:00:02 +0200

    2024-07-05 10:00:01 | M122 Scripts (Auto Update) | Last File: finance

--- finance/fetch_exchange_rate.sh
+++ finance/fetch_exchange_rate.sh
@@ -11,6 +11,9 @@ amount=$1
 from_currency=$2
 to_currency=$3
 
+# Log-Pfad
+log_file="/home/rooney/Docs/logs/finance/exchange_${from_currency}${to_currency}.log"
+
 # API-Host und Endpunkt
 host="api.frankfurter.app"
 endpoint="/latest?amount=${amount}&from=${from_currency}&to=${to_currency}"
@@ -21,7 +24,7 @@ response=$(curl -s "https://${host}${endpoint}")
 # Überprüfen, ob die Anfrage erfolgreich war
 if [ $? -ne 0 ]; then
   echo "Fehler bei der Anfrage an die API"
-  echo "$(date) - Fehler bei der Anfrage an die API" >> /home/rooney/scripts/finance/exchange_rate.log
+  echo "$(date) - Fehler bei der Anfrage an die API" >> $log_file
   exit 1
 fi
 
@@ -31,11 +34,11 @@ rate=$(echo "$response" | grep -oP '"'"${to_currency}"'":\K[0-9.]+')
 # Überprüfen, ob der Ziel-Wechselkurs erfolgreich extrahiert wurde
 if [ -z "$rate" ]; then
   echo "Fehler beim Extrahieren des Wechselkurses"
-  echo "$(date) - Fehler beim Extrahieren des Wechselkurses" >> /home/rooney/scripts/finance/exchange_rate.log
+  echo "$(date) - Fehler beim Extrahieren des Wechselkurses" >> $log_file
   exit 1
 fi
 
 # Ergebnis anzeigen und in die Log-Datei schreiben
 result="${amount} ${from_currency} = ${rate} ${to_currency}"
 echo "$result"
-echo "$(date) - $result" >> /home/rooney/scripts/finance/exchange_rate.log
+echo "$(date) - $result" >> $log_file
