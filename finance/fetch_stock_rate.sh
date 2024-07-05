9cff6bc3abfc8c6a85c01549b8470f16c5863c22
rooney <ronny.bruhin@edu.tbz.ch>
2024-07-05 10:39:57 +0200

    2024-07-05 | fetch_stock_rate.sh v1.2

--- finance/fetch_stock_rate.sh
+++ finance/fetch_stock_rate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Pfad zur Logdatei
-LOG_FILE="/home/rooney/Docs/logs/finance/exchange_CHFUSD.log"
+# Verzeichnis für Logdateien
+LOG_DIR="/home/rooney/Docs/logs/finance"
 
 # Funktion zum Extrahieren des neuesten Wechselkurses aus der Logdatei
 fetch_exchange_rate() {
     # Den neuesten Wechselkurs aus der Logdatei holen (letzte Zeile)
-    EXCHANGE_RATE=$(tail -n 1 "$LOG_FILE" | awk '{print $(NF-1)}')
+    EXCHANGE_RATE=$(tail -n 1 "$LOG_DIR/exchange_CHFUSD.log" | awk '{print $(NF-1)}')
     echo $EXCHANGE_RATE
 }
 
@@ -41,6 +41,19 @@ calculate_stock_value_in_chf() {
     echo $STOCK_VALUE_CHF
 }
 
+# Funktion zum Loggen der Ergebnisse in eine Datei
+log_results() {
+    SYMBOL=$1
+    STOCK_PRICE=$2
+    EXCHANGE_RATE=$3
+    STOCK_VALUE_CHF=$4
+    LOG_FILE="$LOG_DIR/${SYMBOL}_fetch_stock_rate.log"
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - Aktueller Preis von $SYMBOL: $STOCK_PRICE USD" >> "$LOG_FILE"
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - Letzter bekannter Wechselkurs CHF zu USD: $EXCHANGE_RATE" >> "$LOG_FILE"
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - Wert einer $SYMBOL Aktie in CHF: $STOCK_VALUE_CHF" >> "$LOG_FILE"
+    echo "Logdatei für $SYMBOL wurde aktualisiert: $LOG_FILE"
+}
+
 # Hauptteil des Skripts
 
 # Benutzereingabe für das Aktiensymbol und das Startdatum
@@ -62,6 +75,5 @@ fi
 # Wert einer Aktie in CHF berechnen
 STOCK_VALUE_CHF=$(calculate_stock_value_in_chf $STOCK_PRICE $EXCHANGE_RATE)
 
-echo "Aktueller Preis von $SYMBOL: $STOCK_PRICE USD"
-echo "Letzter bekannter Wechselkurs CHF zu USD: $EXCHANGE_RATE"
-echo "Wert einer $SYMBOL Aktie in CHF: $STOCK_VALUE_CHF"
+# Ergebnisse in Logdatei schreiben
+log_results $SYMBOL $STOCK_PRICE $EXCHANGE_RATE $STOCK_VALUE_CHF
